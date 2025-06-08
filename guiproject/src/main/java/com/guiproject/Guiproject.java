937e2eb07f1c31117beca84b5b4137621fc42055
Loug522 <lucianoguasque@gmail.com>
2025-06-07 21:46:04 -0300

    first commit

---
+++ guiproject/src/main/java/com/guiproject/Guiproject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.guiproject;
+
+/**
+ *
+ * @author User
+ */
+public class Guiproject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
