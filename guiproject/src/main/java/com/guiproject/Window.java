1d2af751929c52b60681f07e4808239e90160f8a
Loug522 <lucianoguasque@gmail.com>
2025-06-09 00:41:03 -0300

    improved the window and added a simple button

--- guiproject/src/main/java/com/guiproject/Window.java
+++ guiproject/src/main/java/com/guiproject/Window.java
@@ -1,17 +1,28 @@
 package com.guiproject;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 
 public class Window {
-    
-    JFrame window = new JFrame();
-    
-    public Window() {
-        window.setVisible(true);
-        window.setName("Super Mario World");
-        window.setBounds(0, 0, 600, 400);
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    private JFrame window;
+    private JButton button;
+    private int height;
+    private int width;
 
+    public Window(int height, int width) {
+        window = new JFrame();
+        button = new JButton("Click Me!");
+        this.height = height;
+        this.width = width;
+    }
+      
+    
+    public void Window() {
+        window.setSize(width, height);
+        window.setTitle("Platformer");
+        window.add(button);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         window.setVisible(true);
     }
 }
