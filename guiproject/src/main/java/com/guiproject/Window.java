398da66f6a3c4a991ce666cb167d2feb58a0acaa
Loug522 <lucianoguasque@gmail.com>
2025-06-09 01:07:00 -0300

    figuring out how to move components around

--- guiproject/src/main/java/com/guiproject/Window.java
+++ guiproject/src/main/java/com/guiproject/Window.java
@@ -3,7 +3,6 @@ package com.guiproject;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-
 public class Window {
     private JFrame window;
     private JButton button;
@@ -12,17 +11,23 @@ public class Window {
 
     public Window(int height, int width) {
         window = new JFrame();
-        button = new JButton("Click Me!");
+        button = new JButton("CLICK ME!");
         this.height = height;
         this.width = width;
     }
-      
     
     public void Window() {
+        // BUTTON
+        window.add(button);
+        button.setBounds(250, 275, 100, 50);
+        
+        
+        // WINDOW
+        window.setLayout(null); // This will allow resizing of components
         window.setSize(width, height);
         window.setTitle("Platformer");
-        window.add(button);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
          window.setVisible(true);
     }
 }
